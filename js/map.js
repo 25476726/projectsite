e925936f9c15fe868f41a68e32d56ba3729da59b
25476726 <25476726@edgehill.ac.uk>
2025-02-08 19:01:55 +0000

    fixing map 2

--- js/map.js
+++ js/map.js
@@ -1,8 +1,11 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 53.4, lng: -2.983 },
+        center: { lat: 53.4, lng: -2.983 }, // Liverpool Coordinates
         zoom: 15,
     });
+
+    // Add more map functionality here if needed, like markers or places.
 }
 
+// Make sure initMap is globally accessible
 window.initMap = initMap;
