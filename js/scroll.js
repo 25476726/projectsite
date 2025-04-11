72201524b3e8f9d15c123472b553cffdeda5b8a8
Lewis <lew.brougham@gmail.com>
2025-04-11 13:05:42 +0100

    reverted back as hated the horizontal scroll

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('wheel', function(event) {
-    if (event.deltaY !== 0) {
-        const scrollWrapper = document.querySelector('.scroll-wrapper');
-        scrollWrapper.scrollLeft += event.deltaY;
-        event.preventDefault();
-    }
-}, { passive: false });
